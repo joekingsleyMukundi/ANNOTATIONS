e5f3f4b25e01cb6e19f77fdbb30d41b5e632bcf0
joekingsley mukundi <joekingsleymukundi@gmail.com>
2022-08-01 23:57:50 +0300

    Update BeanConfig.java

--- src/main/java/demo2/BeanConfig.java
+++ src/main/java/demo2/BeanConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 //using configuration file sto do depedancy injections and inversion of controll
 @Configuration
 @ComponentScan(basePackages = "demo2")
+//when geting values from propertiesfile
+//@PropertySource("classpath:sport.properties
+//now where you want tp pass thisvalies
+//@Value("${foo.team}")
+//private String team
+// note the last two lines are supposed to be in the class youdefined them
 public class BeanConfig {
     /**
      * creating beans in the config without component scan
